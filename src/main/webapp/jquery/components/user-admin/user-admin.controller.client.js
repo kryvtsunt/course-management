8a50abba60e4cf1383c6905d004f492f6d5e4eb2
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-11 16:59:57 -0400

    added create user functionality

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,24 +1,45 @@
-
 var tbody;
 var template;
 var users;
 
 //IIFE = Immediately-invoked function expression
-(function(){
-	template = $('.template');
-	tbody = $('tbody');
-	var promise = fetch('http://localhost:8080/api/user');
-	promise.then(function(response){
-		return response.json();
-		}).then(renderUsers)
+(function () {
+    tbody = $('tbody');
+    template = $('.template');
+    $('#createUser').click(createUser);
+    var promise = fetch('http://localhost:8080/api/user');
+    promise.then(function (response) {
+        return response.json();
+    }).then(renderUsers)
 })()
 
 function renderUsers(users) {
-	for (var i=0; i<users.length; i++){
-		var user = users[i];
-		var clone = template.clone();
-		clone.find('.username')
-				.html(user.username);
-		tbody.append(clone)
-	}
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        var clone = template.clone();
+        clone.find('.username')
+            .html(user.username);
+        tbody.append(clone)
+    }
+}
+
+function createUser() {
+    var username = $('#usernameFld').val();
+    var password = $('#passwordFld').val();
+    var firstName = $('#firstNameFld').val();
+    var lastName = $('#lastNameFld').val();
+    var user = {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    };
+
+    fetch('http://localhost:8080/api/user', {
+        method: 'post',
+        body: JSON.stringify(user),
+        headers: {
+            'content-type' : 'application/json'
+        }
+    });
 }
