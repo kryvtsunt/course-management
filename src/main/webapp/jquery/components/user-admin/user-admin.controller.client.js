88cc6cb9145dd1fd51bfa4dc3fad0c77d946aa20
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-13 21:56:50 -0400

    added functionality to user-admin controller

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,45 +1,20 @@
+var userService = new UserServiceClient();
 var tbody;
 var template;
-var users;
-var userService = new UserServiceClient();
+var $usernameFld, $passwordFld;
+var $removeBtn, $editBtn, $createBtn;
+var $firstNameFld, $lastNameFld;
+var $userRowTemplate, $tbody;
+
 
 //IIFE = Immediately-invoked function expression
+// Main function
 (function () {
     tbody = $('tbody');
     template = $('.template');
     $('#createUser').click(createUser);
     findAllUsers();
 })()
-
-function renderUsers(users) {
-    tbody.empty();
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i];
-        var clone = template.clone();
-        clone.attr('id', user.id);
-        clone.find('.delete').click(deleteUser);
-        clone.find('.edit').click(editUser);
-        clone.find('.username')
-            .html(user.username)
-        tbody.append(clone)
-    }
-}
-
-function deleteUser(event) {
-    var deleteBtn = $(event.currentTarget);
-    var userId = deleteBtn
-        .parent()
-        .parent()
-        .attr('id');
-
-    userService
-        .deleteUser(userId)
-        .then(findAllUsers);
-}
-
-function editUser(event) {
-
-}
 
 function createUser() {
     var username = $('#usernameFld').val();
@@ -55,10 +30,57 @@ function createUser() {
     userService.createUser(user)
         .then(findAllUsers);
 
-
 }
-
 
 function findAllUsers() {
     userService.findAllUsers().then(renderUsers);
 }
+
+function findUserById() {
+    //TODO
+}
+
+function deleteUser(event) {
+    var deleteBtn = $(event.currentTarget);
+    var userId = deleteBtn
+        .parent()
+        .parent()
+        .attr('id');
+
+    userService
+        .deleteUser(userId)
+        .then(findAllUsers);
+}
+
+function selectUser() {
+    //TODO
+}
+
+function updateUser(event) {
+    //TODO
+}
+
+function renderUser(user) {
+    //TODO
+}
+
+function renderUsers(users) {
+    tbody.empty();
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        var clone = template.clone();
+        clone.attr('id', user.id);
+        clone.find('.delete').click(deleteUser);
+        clone.find('.edit').click(editUser);
+        clone.find('.username')
+            .html(user.username)
+        tbody.append(clone)
+    }
+}
+
+
+
+
+
+
+
