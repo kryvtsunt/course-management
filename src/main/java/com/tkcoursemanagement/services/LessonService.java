dc5f381f6944f93331fa32fbe8ffcf1686be9c0e
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-22 23:10:41 -0400

    added findAll and createLesson in LessonService at server side

--- src/main/java/com/tkcoursemanagement/services/LessonService.java
+++ src/main/java/com/tkcoursemanagement/services/LessonService.java
@@ -4,17 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tkcoursemanagement.models.Course;
 import com.tkcoursemanagement.models.Lesson;
 import com.tkcoursemanagement.models.Module;
 import com.tkcoursemanagement.repositories.CourseRepository;
 import com.tkcoursemanagement.repositories.LessonRepository;
 import com.tkcoursemanagement.repositories.ModuleRepository;
 
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LessonService {
 	@Autowired
 	CourseRepository courseRepository;
@@ -22,25 +28,22 @@ public class LessonService {
 	ModuleRepository moduleRepository;
 	@Autowired
 	LessonRepository lessonRepository;
-	
-	@GetMapping("api/module/{moduleId}/lesson")
-	public List<Lesson> findAllLessons(
-			@PathVariable("moduleId") int moduleId) {
+
+	@GetMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public List<Lesson> findAllLessons(@PathVariable("courseId") int courseId, @PathVariable("moduleId") int moduleId) {
 		Optional<Module> data = moduleRepository.findById(moduleId);
-		if(data.isPresent()) {
+		if (data.isPresent()) {
 			Module module = data.get();
 			return module.getLessons();
 		}
-		return null;		
+		return null;
 	}
 
-	
-	@PostMapping("/api/module/{moduleId}/lesson/{lessonId}")
-	public Lesson createLesson(
-			@PathVariable("moduleId") int moduleId,
+	@PostMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public Lesson createLesson(@PathVariable("courseId") int courseId, @PathVariable("moduleId") int moduleId,
 			@RequestBody Lesson newLesson) {
 		Optional<Module> data = moduleRepository.findById(moduleId);
-		if(data.isPresent()) {
+		if (data.isPresent()) {
 			Module module = data.get();
 			newLesson.setModule(module);
 			return lessonRepository.save(newLesson);
--- src/main/java/com/tkcoursemanagement/services/ModuleService.java
+++ src/main/java/com/tkcoursemanagement/services/ModuleService.java
@@ -60,6 +60,5 @@ public class ModuleService {
 			moduleRepository.deleteById(moduleId);
 		}
 	}
-	
 
 }
