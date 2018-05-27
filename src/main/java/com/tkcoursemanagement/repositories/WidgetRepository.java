2e9c7b0189ad3a9058b447b1b7307dac3c4d493a
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-05-27 16:09:01 -0400

    created Widget model, repo, and service

---
+++ src/main/java/com/tkcoursemanagement/repositories/WidgetRepository.java
@@ -0,0 +1,7 @@
+package com.tkcoursemanagement.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tkcoursemanagement.models.Widget;;
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer>{}
---
+++ src/main/java/com/tkcoursemanagement/services/WidgetService.java
@@ -0,0 +1,33 @@
+package com.tkcoursemanagement.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tkcoursemanagement.models.Widget;
+import com.tkcoursemanagement.repositories.WidgetRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetService {
+	
+	@Autowired
+	WidgetRepository widgetRepository;
+	
+	@GetMapping ("/api/widget")
+	public List<Widget> findAllWidgets() {
+		return (List<Widget>) widgetRepository.findAll();
+		
+	}
+	
+	@PostMapping("/api/widget/save")
+	public List<Widget> saveWidgets(List<Widget> widgets) {
+		return (List<Widget>) widgetRepository.saveAll(widgets);
+		
+	}
+}
